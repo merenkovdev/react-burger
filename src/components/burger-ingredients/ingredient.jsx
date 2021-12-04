8f3c38cdc7cebd2bab496956794d2d1502952446
Stas Merenkov <merenkov.dev@gmail.com>
2021-12-04 21:38:04 +0300

    MOD: Компонент ingredient

--- src/components/burger-ingredients/ingredient.jsx
+++ src/components/burger-ingredients/ingredient.jsx
@@ -1,19 +1,23 @@
 import styles from './ingredient.module.css';
 
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import cn from 'classnames';
 
 const Ingredient = (props) => (
-	<div className={styles.item}>
-	{ console.log(props) }
-		<div className={cn(styles.item, 'text text_type_main-default')}>
-			<img src={props.image} alt="" />
+	<div className={ styles.item }>
+		{ console.log(props) }
+		<div className={ cn(styles.imageContainer, 'text text_type_main-default') }>
+			<img className={ styles.image } src={ props.image } alt="" />
+			<Counter count={1} size="default" />
 		</div>
-		<div className={cn(styles.price, 'text text_type_digits-default pt-1 pb-1')}>
- 			<span className="pr-2">{props.price}</span><CurrencyIcon type="primary" />
+		<div className={ cn(styles.price, 'text text_type_digits-default pt-1 pb-1') }>
+ 			<span className="pr-2">
+				{ props.price }
+			</span>
+			<CurrencyIcon type="primary" />
 		</div>
-		<div className={cn(styles.title, 'text text_type_main-default')}>
-			{props.name}
+		<div className={ cn(styles.title, 'text text_type_main-default') }>
+			{ props.name }
 		</div>
 	</div>
 );
