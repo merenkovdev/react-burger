8d04cfe1083fee2d1283cb77a1459ce8c3fbfc5a
Stas Merenkov <merenkov.dev@gmail.com>
2021-12-04 21:39:21 +0300

    MOD: Общий js

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app/app';
+import '@ya.praktikum/react-developer-burger-ui-components';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    Hello!
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
