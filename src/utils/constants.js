820214124e92f166540d42be29b1caf518dfb004
Stas Merenkov <merenkov.dev@gmail.com>
2021-12-16 13:54:04 +0300

    MOD: Новые константы, утилита проверки на пустоту

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_URL = 'https://norma.nomoreparties.space/api/ingredients';
+export const API_ORIGIN = 'https://norma.nomoreparties.space';
+export const API_INGREDIENTS = API_ORIGIN + '/api/ingredients';
+export const API_ORDERS = API_ORIGIN + '/api/orders';
 
 export const MODAL_DETAILS = 'modal-details';
 export const MODAL_ORDER = 'modal-order';
