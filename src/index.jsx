211cf013d17753228b9a2377c6dc7339fa4499a7
Stanislav Merenkov <smerenkov@livemaster.ru>
2021-12-24 11:54:21 +0300

    MOD: Провидер стора

--- src/index.jsx
+++ src/index.jsx
@@ -2,26 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import '@ya.praktikum/react-developer-burger-ui-components';
+
+import StoreProvider from './components/store-provider/store-provider';
+
 import './index.css';
-
-import thunk from 'redux-thunk';
-import { compose, createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { rootReducer } from './services/reducers/index.js';
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={ store }>
+		<StoreProvider>
 			<App />
-		</ Provider>
+		</ StoreProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
