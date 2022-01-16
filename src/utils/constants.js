ec422c7defad23df7ea964a74f8cda5d40b46c7e
Stas Merenkov <merenkov.dev@gmail.com>
2022-01-16 14:23:46 +0300

    MOD: Константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,13 @@
 export const API_ORIGIN = 'https://norma.nomoreparties.space/api/';
 export const API_INGREDIENTS = API_ORIGIN + 'ingredients';
 export const API_ORDERS = API_ORIGIN + 'orders';
+export const API_FORGOT_PASSWORD = API_ORIGIN + 'password-reset';
+export const API_RESET_PASSWORD = API_ORIGIN + 'password-reset/reset';
 
 export const MODAL_DETAILS = 'modal-details';
 export const MODAL_ORDER = 'modal-order';
+
+export const FORM_NAME_LOGIN = 'login';
+export const FORM_NAME_REGISTER = 'register';
+export const FORM_NAME_FORGOT_PASSWORD = 'forgot';
+export const FORM_NAME_RESET_PASSWORD = 'reset';
