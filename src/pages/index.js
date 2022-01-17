abb7df412d7d637c8d737d78d140546770f0c55a
Stas Merenkov <merenkov.dev@gmail.com>
2022-01-17 22:32:57 +0300

    MOD: Импорт страницы

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import LoginPage from './login/login';
 import RegisterPage from './register/register';
 import ForgotPasswordPage from './forgot-password/forgot-password';
 import ResetPasswordPage from './reset-password/reset-password';
+import ProfilePage from './profile/profile';
 
 export {
 	Home,
@@ -10,4 +11,5 @@ export {
 	RegisterPage,
 	ForgotPasswordPage,
 	ResetPasswordPage,
+	ProfilePage,
 };
