f7c8244eb5c457da7487320027831dbcd461bae8
Stanislav Merenkov <merenkov.dev@gmail.com>
2022-03-11 19:21:38 +0300

    MOD: Cypress added

---
+++ cypress/integration/site-is-available.spec.js
@@ -0,0 +1,5 @@
+describe('service is available', function() {
+	it('should be available on https://smerenkov.nomoredomains.work/', function() {
+		cy.visit('https://smerenkov.nomoredomains.work/');
+	});
+});
